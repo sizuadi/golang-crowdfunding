13be4151149fc7153caa989fff84b078c49cfe1d
Adi Siswanto <adi15siswanto@gmail.com>
2023-01-24 11:43:28 +0700

    jwt auth, validate token

--- main.go
+++ main.go
@@ -25,7 +25,24 @@ func main() {
 	authService := auth.NewService()
 	userHandler := handler.NewUserHandler(userService, authService)
 
-	fmt.Printf(authService.GenerateToken(1))
+	validateToken, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyfQ.SE5EUlVdEH9fOPRi1WnkQEgHwvjD7RpTa4MjL0XjJB8")
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+	}
+
+	if validateToken.Valid {
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+	} else {
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+
+	}
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
